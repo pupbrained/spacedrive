ae50cfa7c32c15a52593b8ff773a105898ae5178
Brendan Allan <brendonovich@outlook.com>
2023-05-31 11:56:27 +0200

    make demo mode work

--- apps/web/src/patches.ts
+++ apps/web/src/patches.ts
@@ -1,14 +1,22 @@
-import { wsBatchLink } from '@rspc/client/v2';
+import { Link, wsBatchLink } from '@rspc/client/v2';
 
 const serverOrigin = import.meta.env.VITE_SDSERVER_ORIGIN || 'localhost:8080';
 
+const noopLink = ((o) => ({
+	exec() {},
+	abort() {}
+})) satisfies Link;
+
 globalThis.isDev = import.meta.env.DEV;
-globalThis.rspcLinks = [
-	// TODO
-	// loggerLink({
-	// 	enabled: () => getDebugState().rspcLogger
-	// }),
-	wsBatchLink({
-		url: `ws://${serverOrigin}/rspc/ws`
-	})
-];
+globalThis.rspcLinks =
+	import.meta.env.VITE_SD_DEMO_MODE === 'true'
+		? [noopLink]
+		: [
+				// TODO
+				// loggerLink({
+				// 	enabled: () => getDebugState().rspcLogger
+				// }),
+				wsBatchLink({
+					url: `ws://${serverOrigin}/rspc/ws`
+				})
+		  ];
